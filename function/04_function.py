ab8967beaae3504b6ecb82a7b6c4a48485eabaea
Lakshya <malhotralakshya88@gmail.com>
2026-01-26 16:50:56 +0530

    Even odd checker

---
+++ function/04_function.py
@@ -0,0 +1,10 @@
+#1️⃣ Function banao jo number even ya odd bataye
+# 👉 Hint: if
+
+def EvenOdd(num):
+    if num % 2 == 0 :
+        return (f"{num} is even")
+    else:
+        return (f"{num}  is odd")
+    
+print(EvenOdd(24))
